e1f9e31441b107891d2b4b30033dabf5c77cc811
MauricioFGF <mfgf@cin.ufpe.br>
2023-05-30 10:53:58 -0300

    Added particle effect

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,32 +3,54 @@ import people from "../../assets/people.png";
 import ai from "../../assets/ai.png";
 import "./header.css";
 
-const Header = () => (
-  <div className="gpt3__header section__padding" id="home">
-    <div className="gpt3__header-content">
-      <h1 className="gradient__text">
-        Vamos construir algo incrível com GPT-3 OpenAI
-      </h1>
-      <p>
-        Nossa plataforma API oferece nossos modelos e guias mais recentes para
-        as melhores práticas de segurança.
-      </p>
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import particlesJSON from "../../assets/particlesjs-config.json";
 
-      <div className="gpt3__header-content__input">
-        <input type="email" placeholder="Seu E-mail" />
-        <button type="button">Começar</button>
+const Header = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particlesJSON}
+      />
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Vamos construir algo incrível com GPT-3 OpenAI
+        </h1>
+        <p>
+          Nossa plataforma API oferece nossos modelos e guias mais recentes para
+          as melhores práticas de segurança.
+        </p>
+
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Seu E-mail" />
+          <button type="button">Começar</button>
+        </div>
+
+        <div className="gpt3__header-content__people">
+          <img src={people} />
+          <p>1.600 pessoas solicitaram acesso nas últimas 24 horas</p>
+        </div>
       </div>
 
-      <div className="gpt3__header-content__people">
-        <img src={people} />
-        <p>1.600 pessoas solicitaram acesso nas últimas 24 horas</p>
+      <div className="gpt3__header-image">
+        <img src={ai} />
       </div>
     </div>
-
-    <div className="gpt3__header-image">
-      <img src={ai} />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;
